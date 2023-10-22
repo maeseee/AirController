d53604b7cccd7f0baff89ae7b0a301b3ff714f46
Marcel Mathis <maeseee@gmail.com>
2023-10-22 10:39:32 +0200

    Add HttpRequest

---
+++ src/main/java/org/web/HttpsRequest.java
@@ -0,0 +1,46 @@
+package org.web;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.Optional;
+
+public class HttpsRequest {
+
+    final URI uri;
+
+    public HttpsRequest(URI uri) {
+        this.uri = uri;
+    }
+
+    public Optional<String> sendRequest() {
+        Optional<String> responseFromUrl = Optional.empty();
+        try {
+            //final URI uri = new URI(url);
+            final HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            connection.setRequestMethod("GET");
+
+            final int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                responseFromUrl = Optional.of(readResponseFromServer(connection));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return responseFromUrl;
+    }
+
+    private String readResponseFromServer(HttpURLConnection connection) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+}
