aba462c74e313cf5d80850e2f98b9cced0ce0aa9
Marcel Mathis <maeseee@gmail.com>
2023-10-30 21:23:47 +0100

    Remove unused pin number in function

--- src/main/java/org/gpio/GpioFunction.java
+++ src/main/java/org/gpio/GpioFunction.java
@@ -1,19 +1,9 @@
 package org.gpio;
 
 public enum GpioFunction {
-    DHT22_SENSOR(4),
-    MAIN_SYSTEM(5),
-    HUMIDITY_EXCHANGER(6),
-    UNMAPPED(13),
-    NIGHT_TIME(19);
-
-    private final int pin;
-
-    GpioFunction(int pin) {
-        this.pin = pin;
-    }
-
-    public int getPin() {
-        return pin;
-    }
+    DHT22_SENSOR,
+    MAIN_SYSTEM,
+    HUMIDITY_EXCHANGER,
+    UNMAPPED,
+    NIGHT_TIME
 }
