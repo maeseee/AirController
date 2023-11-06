52867a3058bda01ebaadeec26535a65db62ada40
Marcel Mathis <maeseee@gmail.com>
2023-11-06 21:00:01 +0100

    Add Dht22 Sensor

--- src/main/java/org/airController/gpioAdapter/GpioFunction.java
+++ src/main/java/org/airController/gpioAdapter/GpioFunction.java
@@ -1,7 +1,7 @@
 package org.airController.gpioAdapter;
 
 public enum GpioFunction {
-    DHT22_SENSOR(4),
+    DHT22_SENSOR(7),         // BCM 4,  PIN 7
     MAIN_SYSTEM(21),         // BCM 5,  PIN 29
     HUMIDITY_EXCHANGER(22),  // BCM 6,  PIN 31
     UNMAPPED(23),            // BCM 13, PIN 33
--- src/main/java/org/airController/sensor/IndoorAirMeasurement.java
+++ src/main/java/org/airController/sensor/IndoorAirMeasurement.java
@@ -1,10 +1,14 @@
 package org.airController.sensor;
 
+import org.airController.entities.AirVO;
+import org.airController.gpioAdapter.GpioFunction;
 import org.airController.sensorAdapter.IndoorAirMeasurementObserver;
 import org.airController.sensorAdapter.IndoorAirValues;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -12,11 +16,15 @@ import java.util.concurrent.TimeUnit;
 public class IndoorAirMeasurement implements Runnable {
 
     private final List<IndoorAirMeasurementObserver> observers = new ArrayList<>();
+    private final Dht22 dht22 = new Dht22(GpioFunction.DHT22_SENSOR);
+
+    public IndoorAirMeasurement() throws IOException {
+    }
 
     @Override
     public void run() {
-        // TODO
-        notifyObservers(new IndoorAirValues(null));
+        final Optional<AirVO> indoorAirValues = dht22.refreshData();
+        indoorAirValues.ifPresent(airVO -> notifyObservers(new IndoorAirValues(airVO)));
     }
 
     public void addObserver(IndoorAirMeasurementObserver observer) {
@@ -27,7 +35,7 @@ public class IndoorAirMeasurement implements Runnable {
         observers.forEach(observer -> observer.updateAirMeasurement(indoorAirValues));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final IndoorAirMeasurement indoorAirMeasurement = new IndoorAirMeasurement();
         indoorAirMeasurement.addObserver(System.out::println);
         final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
