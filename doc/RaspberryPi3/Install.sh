5bc7fcc21a0e8302b5f958202b9db01566491962
Marcel Mathis <maeseee@gmail.com>
2023-11-11 07:19:41 +0100

    Split run and install

---
+++ doc/RaspberryPi3/Install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cd || exit
+
+# Install some packages
+sudo apt install maven
+sudo apt install openjdk-17-jdk
+sudo apt remove openjdk-11-jre-headless
+
+# Checkout AirController
+git clone https://github.com/maeseee/AirController.git
+
+# Checkout and install wiringpi
+sudo apt-get --yes install git-core gcc make
+git clone https://github.com/WiringPi/WiringPi --branch master --single-branch wiringpi
+cd wiringpi || exit
+sudo ./build
+
+# Set startup pin config (RasperryPi will not run without failure without this config when connected to the PiOT Relay Board)
+sudo tee -a /boot/config.txt << EOF
+dtoverlay=w1-gpio
+gpio=5=op,dh
+gpio=6=op,dl
+gpio=13=op,dl
+gpio=19=op,dl
+EOF
+
+# Config startup behaviour
+cd || exit
+cd AirController || exit
+sudo cp doc/RaspberryPi3/airController.service /etc/systemd/system/
+sudo systemctl enable airController.service # Enable the service to start at boot time
+chmod +x run.sh
+
+# Add the following command to the /home/pi/.bashrc (without "")
+export weather_api_key="<api_key>"
+
+#See GPIO config
+gpio readall
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd ~/AirController || exit
+cd /home/pi/AirController || exit
 mkdir -p "log"
 now=$(date +"%Y_%m_%d")
 log_filename="log/log_${now}.log"
@@ -10,13 +10,6 @@ mvn exec:java -Dexec.mainClass="org.airController.Main" | tee -a "${log_filename
 echo "AirController has been restarted on $(date)" | tee -a "${log_filename}"
 sleep 5s # just sleep a little before restart again
 done
-
-# Add the following command to the /etc/rc.local file
-# /home/pi/AirController/run.sh &
-
-# Add the following command to the /home/pi/.bashrc
-# export weather_api_key=<api_key>
-
 
 # Run application on raspberry pi
 # mvn exec:java -Dexec.mainClass="org.airController.Main"
