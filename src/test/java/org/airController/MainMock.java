1dc62b5254d2bf9b7895821ce826cbbe71075b53
Marcel Mathis <maeseee@gmail.com>
2023-11-18 13:43:23 +0100

    Delete self made mock

--- src/test/java/org/airController/MainMock.java
+++ src/test/java/org/airController/MainMock.java
@@ -3,21 +3,27 @@ package org.airController;
 import org.airController.entities.AirVO;
 import org.airController.gpio.GpioPinMock;
 import org.airController.gpioAdapter.GpioPin;
-import org.airController.sensor.Dht22Mock;
+import org.airController.sensor.Dht22;
 import org.airController.sensor.IndoorSensorImpl;
+import org.airController.sensor.SensorValueImpl;
 import org.airController.sensorAdapter.IndoorSensor;
+import org.airController.sensorAdapter.SensorValue;
 
 import java.net.URISyntaxException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class MainMock {
 
     public static void main(String[] args) throws InterruptedException, URISyntaxException {
         final GpioPin airFlow = new GpioPinMock("AirFlow");
         final GpioPin humidityExchanger = new GpioPinMock("HumidityExchanger");
-        final Dht22Mock dht22Mock = new Dht22Mock();
         final AirVO airVo = new AirVO(23.0, 50.0);
-        dht22Mock.setData(airVo);
-        final IndoorSensor indoorSensor = new IndoorSensorImpl(dht22Mock);
+        final SensorValue sensorValue = new SensorValueImpl(airVo);
+        final Dht22 dht22 = mock(Dht22.class);
+        when(dht22.refreshData()).thenReturn(sensorValue);
+        final IndoorSensor indoorSensor = new IndoorSensorImpl(dht22);
         final Application application = new Application(airFlow, humidityExchanger, indoorSensor);
         application.run();
         Thread.currentThread().join();
