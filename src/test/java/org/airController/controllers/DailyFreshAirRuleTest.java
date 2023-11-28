f7b07257fd64a1af715df868cd20aa1b9500630a
Marcel Mathis <maeseee@gmail.com>
2023-11-28 20:49:16 +0100

    Delete duplicated test

--- src/test/java/org/airController/controllers/DailyFreshAirRuleTest.java
+++ src/test/java/org/airController/controllers/DailyFreshAirRuleTest.java
@@ -17,7 +17,15 @@ class DailyFreshAirRuleTest {
             "2023-12-24 14:58, true",
             "2023-07-24 14:58, false",
             "2023-08-05 23:45, false",
-            "2023-08-13 02:34, true"
+            "2023-08-13 02:34, true",
+            "2023-01-01 17:30, false",
+            "2023-01-01 13:00, false",
+            "2023-01-01 13:01, true",
+            "2023-01-01 16:30, true",
+            "2023-08-01 07:30, false",
+            "2023-08-01 01:00, false",
+            "2023-08-01 02:01, true",
+            "2023-08-01 05:30, true"
     })
     void testDates(String dateTimeString, boolean expectedResult) {
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
