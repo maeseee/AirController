289d8f59c36f5d66c1bc9f4574a6016db22d4ed5
Marcel Mathis <maeseee@gmail.com>
2024-09-18 20:26:41 +0200

    Delete old daily report

--- src/main/java/org/airController/gpio/GpioPinImpl.java
+++ src/main/java/org/airController/gpio/GpioPinImpl.java
@@ -7,14 +7,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.time.LocalTime;
 
 public class GpioPinImpl implements GpioPin {
     private static final Logger logger = LogManager.getLogger(GpioPinImpl.class);
 
     private final String name;
     private final RaspberryPiPin raspberryPiPin;
-    private final DailyGpioStatistic dailyGpioStatistic; // TODO move
 
     public GpioPinImpl(GpioFunction pinFunction, boolean initialHigh) throws IOException {
         this(pinFunction.name(), new RaspberryPiPin(pinFunction), initialHigh);
@@ -23,7 +21,6 @@ public class GpioPinImpl implements GpioPin {
     public GpioPinImpl(String name, RaspberryPiPin raspberryPiPin, boolean initialHigh) {
         this.name = name;
         this.raspberryPiPin = raspberryPiPin;
-        this.dailyGpioStatistic = new DailyGpioStatistic(name, initialHigh);
 
         raspberryPiPin.export(true);
         logger.info("{} set initial to {}", name, initialHigh ? "on" : "off");
@@ -37,7 +34,6 @@ public class GpioPinImpl implements GpioPin {
 
     @Override
     public void setGpioState(boolean stateOn) {
-        dailyGpioStatistic.stateChange(stateOn, LocalTime.now());
         if (getGpioState() != stateOn) {
             logger.info("{} set to {}", name, stateOn ? "on" : "off");
             raspberryPiPin.write(stateOn);
