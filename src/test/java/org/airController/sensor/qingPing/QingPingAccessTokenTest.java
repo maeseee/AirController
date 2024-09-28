626a27a5896ec01e5fe1f7fc8d23c1012a88de2a
Marcel Mathis <maeseee@gmail.com>
2024-09-28 12:48:20 +0200

    Add test for bad request

---
+++ src/test/java/org/airController/sensor/qingPing/QingPingAccessTokenTest.java
@@ -0,0 +1,24 @@
+package org.airController.sensor.qingPing;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class) class QingPingAccessTokenTest {
+
+    @Mock
+    private QingPingAccessTokenRequest request;
+
+    @Test void shouldThrow_WhenRequestFails() {
+        when(request.sendRequest()).thenReturn(Optional.empty());
+        final QingPingAccessToken testee = new QingPingAccessToken(request);
+
+        assertThatExceptionOfType(CommunicationException.class).isThrownBy(testee::readToken);
+    }
+}
