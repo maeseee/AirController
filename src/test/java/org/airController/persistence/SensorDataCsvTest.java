e7c488a7df5a0c428b0abce30e60f25beeefab92
Marcel Mathis <maeseee@gmail.com>
2024-10-06 20:08:08 +0200

    Fix warnings

--- src/test/java/org/airController/persistence/SensorDataCsvTest.java
+++ src/test/java/org/airController/persistence/SensorDataCsvTest.java
@@ -1,40 +1,42 @@
 package org.airController.persistence;
 
 import org.airController.sensorValues.*;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 class SensorDataCsvTest {
 
-    private final String FILE_PATH = "log/sensorValueCsvWriterTest.csv";
-
     @Test
-    void shouldWriteSensorDataIntoCsvFiles_whenPersist() throws InvalidArgumentException, IOException {
+    void shouldWriteSensorDataIntoCsvFiles_whenPersist() throws InvalidArgumentException {
         final Random random = new Random();
         final double celsiusTemperature = random.nextDouble() * 100;
         final double relativeHumidity = random.nextDouble() * 100;
         final double co2Ppm = random.nextDouble() * 100000;
         final LocalDateTime time = LocalDateTime.of(2024, 9, 27, 20, 51, 12);
         final SensorData inputSensorData = createSensorData(celsiusTemperature, relativeHumidity, co2Ppm, time);
-        final SensorDataPersistence testee = new SensorDataCsv(FILE_PATH);
+        final String csvFilePath = "log/sensorValueCsvWriterTest.csv";
+        final SensorDataPersistence testee = new SensorDataCsv(csvFilePath);
 
         testee.persist(inputSensorData);
         final List<SensorData> sensorDataList = testee.read();
 
         final SensorData lastSensorData = sensorDataList.get(sensorDataList.size() - 1);
         assertThat(lastSensorData.getTemperature()).isPresent().hasValueSatisfying(
-                temperature -> assertThat(temperature.getCelsius()).isEqualTo(celsiusTemperature));
+                temperature -> assertThat(temperature.getCelsius()).isCloseTo(temperature.getCelsius(), Offset.offset(0.001)));
         assertThat(lastSensorData.getHumidity()).isPresent().hasValueSatisfying(
-                humidity -> assertThat(humidity.getRelativeHumidity(lastSensorData.getTemperature().get())).isEqualTo(relativeHumidity));
+                humidity -> assertThat(humidity.getRelativeHumidity(lastSensorData.getTemperature().get())).isCloseTo(relativeHumidity,
+                        Offset.offset(0.001)));
         assertThat(lastSensorData.getCo2()).isPresent().hasValueSatisfying(
-                co2 -> assertThat(co2.getPpm()).isEqualTo(co2Ppm));
-        assertThat(lastSensorData.getTimeStamp()).isEqualTo(time);
+                co2 -> assertThat(co2.getPpm()).isCloseTo(co2Ppm, Offset.offset(0.001)));
+        assertThat(lastSensorData.getTimeStamp()).isCloseTo(time, within(1, ChronoUnit.SECONDS));
     }
 
     private SensorData createSensorData(double celsiusTemperatur, double relativeHumidity, double co2Ppm, LocalDateTime time)
