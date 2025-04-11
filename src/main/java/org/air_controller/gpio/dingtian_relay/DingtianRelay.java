661ece920062c74e3103f7a5a7b13829d4a9c2ed
Marcel Mathis <maeseee@gmail.com>
2025-04-11 09:11:09 +0200

    Implement DingtianRelay

---
+++ src/main/java/org/air_controller/gpio/dingtian_relay/DingtianRelay.java
@@ -0,0 +1,35 @@
+package org.air_controller.gpio.dingtian_relay;
+
+import org.air_controller.http.HttpsGetRequest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
+
+public class DingtianRelay {
+    private static final String RELAY_URL = "http://192.168.50.22";
+    private static final String GET_RELAY_STATE_URL = RELAY_URL + "/relay_cgi_load.cgi";
+
+    private final HttpsGetRequest httpsGetRequest;
+    private final DingtianResponseInterpreter interpreter;
+
+    public DingtianRelay() throws URISyntaxException {
+        this(new HttpsGetRequest(new URI(GET_RELAY_STATE_URL)), new DingtianResponseInterpreter());
+    }
+
+    DingtianRelay(HttpsGetRequest httpsGetRequest, DingtianResponseInterpreter interpreter) {
+        this.httpsGetRequest = httpsGetRequest;
+        this.interpreter = interpreter;
+    }
+
+    public List<Boolean> readStates() {
+        final Optional<String> response = httpsGetRequest.sendRequest();
+        if (response.isEmpty()) {
+            return emptyList();
+        }
+        return interpreter.interpretRelayState(response.get());
+    }
+}
---
+++ src/test/java/org/air_controller/gpio/dingtian_relay/DingtianRelayTest.java
@@ -0,0 +1,55 @@
+package org.air_controller.gpio.dingtian_relay;
+
+import org.air_controller.http.HttpsGetRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class DingtianRelayTest {
+    @Mock
+    private HttpsGetRequest getRequest;
+    @Mock
+    private DingtianResponseInterpreter interpreter;
+
+    @Test
+    void shouldReadRelayStates() throws URISyntaxException {
+        final DingtianRelay testee = new DingtianRelay();
+
+        final List<Boolean> relayStates = testee.readStates();
+
+        assertThat(relayStates).isNotEmpty();
+    }
+
+    @Test
+    void shouldReturnEmptyList_whenResponseEmpty() {
+        when(getRequest.sendRequest()).thenReturn(Optional.empty());
+        final DingtianRelay testee = new DingtianRelay(getRequest, interpreter);
+
+        final List<Boolean> relayStates = testee.readStates();
+
+        assertThat(relayStates).isEmpty();
+        verify(getRequest).sendRequest();
+        verifyNoInteractions(interpreter);
+    }
+
+    @Test
+    void shouldInterpretResponse_whenValid() {
+        when(getRequest.sendRequest()).thenReturn(Optional.of("RESPONSE"));
+        final DingtianRelay testee = new DingtianRelay(getRequest, interpreter);
+
+        final List<Boolean> relayStates = testee.readStates();
+
+        assertThat(relayStates).isEmpty();
+        verify(getRequest).sendRequest();
+        verify(interpreter).interpretRelayState("RESPONSE");
+    }
+}
