14cac0d94e587e6e0e5c2ae21f9625ba90350f6e
Marcel Mathis <maeseee@gmail.com>
2025-05-26 21:21:52 +0200

    Use MariaDB on my synology

--- src/main/java/org/air_controller/persistence/Persistence.java
+++ src/main/java/org/air_controller/persistence/Persistence.java
@@ -10,13 +10,13 @@ import java.sql.SQLException;
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class Persistence {
     private static final String SCHEMA_NAME = "AirControllerSystem";
-    private static final String JDBC_URL = "jdbc:h2:./" + SCHEMA_NAME;
-    private static final String USER = "SensorData"; // TODO update username
-    private static final String ENVIRONMENT_VARIBLE_DB = "dbPassword";
-    private static final String ENCRYPTED_DB_SECRET = "mMwIpBLqf8oVg+ahrUTiKRRjx/hdEffKEw6klDCNY3c=";
+    private static final String JDBC_URL = "jdbc:mariadb://DS224P.local:3306/" + SCHEMA_NAME;
+    private static final String USER = "mariaDb";
+    private static final String ENVIRONMENT_VARIABLE_DB = "mariaDdPassword";
+    private static final String ENCRYPTED_DB_SECRET = "cfKfHZKIpDP3vLkTZcwxcMYUFW5DFnXSrkjCrlqmM9U=";
 
     public static Connection createConnection() throws SQLException {
-        final String password = Secret.getSecret(ENVIRONMENT_VARIBLE_DB, ENCRYPTED_DB_SECRET);
+        final String password = Secret.getSecret(ENVIRONMENT_VARIABLE_DB, ENCRYPTED_DB_SECRET);
         return DriverManager.getConnection(JDBC_URL, USER, password);
     }
 }
