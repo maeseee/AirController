2daf048167ab362a236151f1d184f4cd2942fe51
Marcel Mathis <maeseee@gmail.com>
2025-06-01 20:55:41 +0200

    Use interface

--- src/main/java/org/air_controller/rules/RuleApplierBuilder.java
+++ src/main/java/org/air_controller/rules/RuleApplierBuilder.java
@@ -3,19 +3,18 @@ package org.air_controller.rules;
 import org.air_controller.sensor.Sensors;
 import org.air_controller.sensor_values.CurrentSensorData;
 import org.air_controller.system.VentilationSystem;
-import org.air_controller.system.VentilationSystemTimeKeeper;
 
 import java.util.List;
 
 public class RuleApplierBuilder {
 
-    public RuleApplier build(List<VentilationSystem> ventilationSystems, Sensors sensors, VentilationSystemTimeKeeper timeKeeper) {
+    public RuleApplier build(List<VentilationSystem> ventilationSystems, Sensors sensors, TimeKeeper timeKeeper) {
         final List<Rule> freshAirRules = getFreshAirRules(sensors, timeKeeper);
         final List<Rule> exchangeHumidityRules = getHumidityExchangeRules(sensors);
         return new RuleApplier(ventilationSystems, freshAirRules, exchangeHumidityRules);
     }
 
-    private List<Rule> getFreshAirRules(Sensors sensors, VentilationSystemTimeKeeper timeKeeper) {
+    private List<Rule> getFreshAirRules(Sensors sensors, TimeKeeper timeKeeper) {
         final CurrentSensorData currentOutdoorSensorData = new CurrentSensorData(sensors.outdoor().getPersistence());
         final CurrentSensorData currentIndoorSensorData = new CurrentSensorData(sensors.indoor().getPersistence());
         final CO2ControlAirFlow co2ControlAirFlow = new CO2ControlAirFlow(currentIndoorSensorData);
