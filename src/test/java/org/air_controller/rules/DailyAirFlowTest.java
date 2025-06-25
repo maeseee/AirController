fa28530c6bb18ddd509feaadd32fbd370e214996
Marcel Mathis <maeseee@gmail.com>
2025-06-25 20:11:15 +0200

    Add tests for transactional season

--- src/test/java/org/air_controller/rules/DailyAirFlowTest.java
+++ src/test/java/org/air_controller/rules/DailyAirFlowTest.java
@@ -28,6 +28,32 @@ class DailyAirFlowTest {
             "2023-01-24 08:00 Z, 0.0",
             "2023-01-24 20:00 Z, 0.0"
     })
+    void shouldCalculateFreshAirConfidenceAccordingToTime(String dateTimeString, double expectedResult) {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm X");
+        final ZonedDateTime dateTime = ZonedDateTime.parse(dateTimeString, formatter);
+        final DailyAirFlow testee = new DailyAirFlow();
+        try (MockedStatic<ZonedDateTime> mocked = mockStatic(ZonedDateTime.class)) {
+            mocked.when(() -> ZonedDateTime.now(ZoneOffset.UTC)).thenReturn(dateTime);
+
+            final Confidence result = testee.turnOnConfidence();
+
+            assertThat(result.getWeightedConfidenceValue()).isCloseTo(expectedResult * 0.6, within(0.01));
+        }
+    }
+
+    @ParameterizedTest(name = "{index} => dateTime={0}, expectedResult={1}")
+    @CsvSource({
+            "2023-05-10 02:00 Z, 0.0",
+            "2023-05-20 02:00 Z, 0.5",
+            "2023-05-30 02:00 Z, 1",
+            "2023-04-30 02:00 Z, -0.5",
+            "2023-04-20 02:00 Z, -1.0",
+            "2023-09-21 02:00 Z, 0.0",
+            "2023-10-01 02:00 Z, -0.5",
+            "2023-10-11 02:00 Z, -1.0",
+            "2023-09-11 02:00 Z, 0.5",
+            "2023-09-01 02:00 Z, 1.0"
+    })
     void shouldCalculateFreshAirConfidenceAccordingToSeason(String dateTimeString, double expectedResult) {
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm X");
         final ZonedDateTime dateTime = ZonedDateTime.parse(dateTimeString, formatter);
