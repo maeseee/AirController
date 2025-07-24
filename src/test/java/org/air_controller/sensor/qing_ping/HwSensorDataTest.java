83e16e6bb5ed1b897c7193f2cdd607d72a7d430f
Marcel Mathis <maeseee@gmail.com>
2025-07-24 20:39:22 +0200

    Parameterize test

--- src/test/java/org/air_controller/sensor/qing_ping/HwSensorDataTest.java
+++ src/test/java/org/air_controller/sensor/qing_ping/HwSensorDataTest.java
@@ -4,57 +4,32 @@ import org.air_controller.sensor_values.CarbonDioxide;
 import org.air_controller.sensor_values.Humidity;
 import org.air_controller.sensor_values.InvalidArgumentException;
 import org.air_controller.sensor_values.Temperature;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.ZonedDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class HwSensorDataTest {
 
-    @Test
-    void shouldBeEquals_whenAllElementsAreEqual() throws InvalidArgumentException {
+    @ParameterizedTest
+    @CsvSource({
+            "23, 50, 500, 0, true",
+            "24, 50, 500, 0, false",
+            "23, 60, 500, 0, false",
+            "23, 50, 600, 0, false",
+            "23, 50, 500, 1, false",
+    })
+    void shouldBeDifferent(double comparedTemperature, double comparedHumidity, int comparedCo2, int timeOffset, boolean isEquals)
+            throws InvalidArgumentException {
         final ZonedDateTime timestamp = ZonedDateTime.now();
-        final HwSensorData testee1 = createHwSensorData(23.0, 50.0, 500, timestamp);
-        final HwSensorData testee2 = createHwSensorData(23.0, 50.0, 500, timestamp);
+        final HwSensorData testee = createHwSensorData(23.0, 50.0, 500, timestamp);
+        final HwSensorData compareObject = createHwSensorData(comparedTemperature, comparedHumidity, comparedCo2, timestamp.plusSeconds(timeOffset));
 
-        final boolean equals = testee1.equals(testee2);
+        final boolean equals = testee.equals(compareObject);
 
-        assertTrue(equals);
-    }
-
-    @Test
-    void shouldBeDifferent_whenTemperatureDifferent() throws InvalidArgumentException {
-        final ZonedDateTime timestamp = ZonedDateTime.now();
-        final HwSensorData testee1 = createHwSensorData(23.0, 50.0, 500, timestamp);
-        final HwSensorData testee2 = createHwSensorData(24.0, 50.0, 500, timestamp);
-
-        final boolean equals = testee1.equals(testee2);
-
-        assertFalse(equals);
-    }
-
-    @Test
-    void shouldBeDifferent_whenHumidityDifferent() throws InvalidArgumentException {
-        final ZonedDateTime timestamp = ZonedDateTime.now();
-        final HwSensorData testee1 = createHwSensorData(23.0, 50.0, 500, timestamp);
-        final HwSensorData testee2 = createHwSensorData(23.0, 60.0, 500, timestamp);
-
-        final boolean equals = testee1.equals(testee2);
-
-        assertFalse(equals);
-    }
-
-    @Test
-    void shouldBeDifferent_whenCo2Different() throws InvalidArgumentException {
-        final ZonedDateTime timestamp = ZonedDateTime.now();
-        final HwSensorData testee1 = createHwSensorData(23.0, 50.0, 500, timestamp);
-        final HwSensorData testee2 = createHwSensorData(23.0, 50.0, 600, timestamp);
-
-        final boolean equals = testee1.equals(testee2);
-
-        assertFalse(equals);
+        assertThat(equals).isEqualTo(isEquals);
     }
 
     private HwSensorData createHwSensorData(double temperatureValue, double humidityValue, int co2Value, ZonedDateTime timestamp)
