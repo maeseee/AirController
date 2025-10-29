20aa74f6981cd43be4d4fec0d6911869d038fbd3
Marcel Mathis <maeseee@gmail.com>
2025-10-29 21:44:43 +0100

    Ged rid of setters in Builder

--- src/main/java/org/air_controller/ApplicationBuilderSharedObjects.java
+++ src/main/java/org/air_controller/ApplicationBuilderSharedObjects.java
@@ -1,6 +1,5 @@
 package org.air_controller;
 
-import lombok.Setter;
 import org.air_controller.gpio.GpioPins;
 import org.air_controller.gpio.dingtian_relay.DingtianPin;
 import org.air_controller.gpio.dingtian_relay.DingtianRelay;
@@ -16,16 +15,27 @@ import org.air_controller.system_action.SystemActionDbAccessor;
 import org.air_controller.system_action.SystemActionDbAccessors;
 import org.air_controller.system_action.SystemActionPersistence;
 import org.air_controller.system_action.SystemPart;
+import org.jetbrains.annotations.VisibleForTesting;
 
 import java.util.List;
 
-@Setter
 class ApplicationBuilderSharedObjects {
     private SystemActionDbAccessors systemActionDbAccessors;
     private List<VentilationSystem> ventilationSystems;
     private CurrentSensors currentSensors;
     private List<Rule> freshAirRules;
     private GpioPins gpioPins;
+
+    public ApplicationBuilderSharedObjects() {
+        this(createSystemActionDbAccessors(), createDingtianPins());
+    }
+
+    @VisibleForTesting
+    public ApplicationBuilderSharedObjects(SystemActionDbAccessors systemActionDbAccessors, GpioPins gpioPins) {
+        this.systemActionDbAccessors = systemActionDbAccessors;
+        this.gpioPins = gpioPins;
+    }
+
 
     public SystemActionDbAccessors getSystemActionDbAccessors() {
         if (systemActionDbAccessors == null) {
@@ -50,24 +60,17 @@ class ApplicationBuilderSharedObjects {
 
     public List<Rule> getFreshAirRules(Sensors sensors) {
         if (freshAirRules == null) {
-            createFreshAirRules(sensors, getDbAccessor(SystemPart.AIR_FLOW));
+            createFreshAirRules(sensors, createDbAccessor(SystemPart.AIR_FLOW));
         }
         return freshAirRules;
     }
 
-    private GpioPins getGpioPins() {
-        if (gpioPins == null) {
-            createDingtianPins();
-        }
-        return gpioPins;
-    }
-
-    private SystemActionDbAccessors createSystemActionDbAccessors() {
-        return new SystemActionDbAccessors(getDbAccessor(SystemPart.AIR_FLOW), getDbAccessor(SystemPart.HUMIDITY));
+    private static SystemActionDbAccessors createSystemActionDbAccessors() {
+        return new SystemActionDbAccessors(createDbAccessor(SystemPart.AIR_FLOW), createDbAccessor(SystemPart.HUMIDITY));
     }
 
     private void createVentilationSystems() {
-        final VentilationSystem ventilationSystem = new ControlledVentilationSystem(getGpioPins());
+        final VentilationSystem ventilationSystem = new ControlledVentilationSystem(gpioPins);
         final SystemActionPersistence systemActionPersistence = new SystemActionPersistence(getSystemActionDbAccessors());
         ventilationSystems = List.of(ventilationSystem, systemActionPersistence);
     }
@@ -82,11 +85,11 @@ class ApplicationBuilderSharedObjects {
         freshAirRules = new FreshAirRuleBuilder().build(getCurrentSensors(sensors), dbAccessor);
     }
 
-    private void createDingtianPins() {
-        gpioPins = new GpioPins(new DingtianPin(DingtianRelay.AIR_FLOW, true), new DingtianPin(DingtianRelay.HUMIDITY_EXCHANGER, false));
+    private static GpioPins createDingtianPins() {
+        return new GpioPins(new DingtianPin(DingtianRelay.AIR_FLOW, true), new DingtianPin(DingtianRelay.HUMIDITY_EXCHANGER, false));
     }
 
-    private SystemActionDbAccessor getDbAccessor(SystemPart systemPart) {
+    private static SystemActionDbAccessor createDbAccessor(SystemPart systemPart) {
         return new SystemActionDbAccessor(new MariaDatabase(), systemPart);
     }
 }
