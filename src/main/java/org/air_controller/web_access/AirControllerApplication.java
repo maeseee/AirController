898fd29d55e8b044f88182c30fba9d9e8056c4a7
Marcel Mathis <maeseee@gmail.com>
2025-12-27 20:58:20 +0100

    Add spring boot

---
+++ src/main/java/org/air_controller/web_access/AirControllerApplication.java
@@ -0,0 +1,13 @@
+package org.air_controller.web_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirControllerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AirControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/air_controller/web_access/AirControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.air_controller.web_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
