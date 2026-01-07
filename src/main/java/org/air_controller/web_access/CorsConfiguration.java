64c27da6b7f46d44be50ffd20bd02116e5cde916
Marcel Mathis <maeseee@gmail.com>
2026-01-07 19:56:22 +0100

    Fix CorsConfiguration

--- src/main/java/org/air_controller/web_access/CorsConfiguration.java
+++ src/main/java/org/air_controller/web_access/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package org.air_controller.web_access;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
