dc28c210f4cb6a7bb3b91098f0c717a4a8502546
marcel <maeseee@gmail.com>
2026-04-04 12:55:44 +0200

    document development computer if linux

---
+++ doc/Development Computer/environment.sh
@@ -0,0 +1,9 @@
+# Used for terminal inputs (e.g. IDE started from terminal)
+echo "export weather_api_key=<api_key>" >> ~/.bashrc
+echo "export qingping_app_secret=<secret>" >> ~/.bashrc
+echo "export mariaDdPassword=<secret>" >> ~/.bashrc
+
+#Used for UI inputs (e.g. IDE started from UI)
+echo "export weather_api_key=<api_key>" >> ~/.profile
+echo "export qingping_app_secret=<secret>" >> ~/.profile
+echo "export mariaDdPassword=<secret>" >> ~/.profile
