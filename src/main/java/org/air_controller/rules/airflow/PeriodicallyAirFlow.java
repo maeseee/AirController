7701631f41d544b48885bbe92224ed2ec66fbccb
marcel <maeseee@gmail.com>
2026-05-01 20:25:10 +0200

    Increase to three-hour period

--- src/main/java/org/air_controller/rules/airflow/PeriodicallyAirFlow.java
+++ src/main/java/org/air_controller/rules/airflow/PeriodicallyAirFlow.java
@@ -15,9 +15,9 @@ import java.util.List;
 @Component
 class PeriodicallyAirFlow implements AirFlowRule {
 
-    private static final Duration TWO_HOURLY_FRESH_AIR = Duration.ofMinutes(60);
+    private static final Duration THREE_HOURLY_FRESH_AIR = Duration.ofMinutes(180);
     private static final double B = 1; // y = xm + b
-    private static final double M = -B / TWO_HOURLY_FRESH_AIR.toMinutes(); // y = xm + b
+    private static final double M = -B / (THREE_HOURLY_FRESH_AIR.toMinutes() / 2.0); // y = xm + b
     private static final double CONFIDENCE_WEIGHT = 0.3;
 
     private final SystemActionDbAccessor dbAccessor;
@@ -28,19 +28,19 @@ class PeriodicallyAirFlow implements AirFlowRule {
 
     @Override
     public String name() {
-        return "Two hourly air flow control";
+        return "Three hourly air flow control";
     }
 
     @Override
     public Confidence turnOnConfidence() {
-        final Duration airFlowOnDurationInLastHour = getOnDurationOfLastTwoHours();
-        final double impact = M * airFlowOnDurationInLastHour.toMinutes() + B;
+        final Duration onDuration = getOnDuration(THREE_HOURLY_FRESH_AIR);
+        final double impact = M * onDuration.toMinutes() + B;
         return Confidence.createWeighted(impact, CONFIDENCE_WEIGHT);
     }
 
-    public Duration getOnDurationOfLastTwoHours() {
+    public Duration getOnDuration(Duration onDuration) {
         final ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
-        final Duration duration = Duration.ofHours(3); // Just enough
+        final Duration duration = onDuration.plus(Duration.ofHours(1)); // Just a little bit more
         final List<SystemAction> actionsFromLastHour = dbAccessor.getActions(duration);
         final DurationCalculator durationCalculator = new DurationCalculator(actionsFromLastHour);
         return durationCalculator.getDuration(now.minus(duration), now);
--- src/test/java/org/air_controller/rules/airflow/PeriodicallyAirFlowTest.java
+++ src/test/java/org/air_controller/rules/airflow/PeriodicallyAirFlowTest.java
@@ -29,8 +29,8 @@ class PeriodicallyAirFlowTest {
     @ParameterizedTest(name = "{index} => onDuration={0}, expectedResult={1}")
     @CsvSource({
             "0, 0.3",
-            "120, -0.3",
-            "60, 0"
+            "180, -0.3",
+            "90, 0"
     })
     void shouldReturnPercentage(int onDuration, double expectedResult) {
         final ZonedDateTime endTime = ZonedDateTime.now(ZoneOffset.UTC);
