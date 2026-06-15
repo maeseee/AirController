36f1897e81ef6a77d30c4f244edd2f362017c20d
marcel <maeseee@gmail.com>
2026-06-15 20:44:51 +0200

    Add Integration test for open weather api

---
+++ src/test/java/org/air_controller/sensor/open_weather_api/OpenWeathoerApiIntegrationTest.java
@@ -0,0 +1,19 @@
+package org.air_controller.sensor.open_weather_api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true")
+class OpenWeatherApiIntegrationTest {
+
+    @Test
+    void shouldCallOpenWeatherApi() {
+        final OpenWeatherApiSensor testee = new OpenWeatherApiSensor();
+
+        final String response = testee.readData();
+
+        assertThat(response).isNotNull();
+    }
+}
