4e95503e04ebc903a18c57fe84931283c33e3258
marcel <maeseee@gmail.com>
2026-06-23 20:45:46 +0200

    Refactor

--- src/test/java/org/air_controller/sensor/moon_phases/MoonPhaseCalculatorTest.java
+++ src/test/java/org/air_controller/sensor/moon_phases/MoonPhaseCalculatorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
@@ -14,18 +13,17 @@ public class MoonPhaseCalculatorTest {
 
     @ParameterizedTest(name = "{index} => fromDateString={0}, expectedNextFullMoonString={1}")
     @CsvSource({
-            "2026-06-23, 2026-06-29 23:56 Z",
-            "2026-12-10, 2026-12-24 01:28 Z",
-            "2024-01-23, 2024-01-25 17:54 Z"
+            "2026-06-23, 2026-06-29T23:56:00Z",
+            "2026-12-10, 2026-12-24T01:28:00Z",
+            "2024-01-23, 2024-01-25T17:54:00Z"
     })
     public void testCalculateMoonPhase(String fromDateString, String expectedNextFullMoonString) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm X");
-        final ZonedDateTime fromDate = ZonedDateTime.parse(fromDateString + " 00:00 Z", formatter);
+        final ZonedDateTime fromDate = ZonedDateTime.parse(fromDateString + "T00:00:00Z");
         final MoonPhaseCalculator testee = new MoonPhaseCalculator();
 
         final ZonedDateTime fullMoon = testee.nextFullMoon(fromDate);
 
-        final ZonedDateTime expectedFullMoon = ZonedDateTime.parse(expectedNextFullMoonString, formatter);
+        final ZonedDateTime expectedFullMoon = ZonedDateTime.parse(expectedNextFullMoonString);
         assertThat(fullMoon).isCloseTo(expectedFullMoon, within(Duration.ofHours(16))); // Toleranz for current calculation
     }
 }
